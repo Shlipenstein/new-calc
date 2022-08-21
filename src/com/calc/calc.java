04aa65a854f9fe5117933997f89eb66eeaf9ce63
Shlipenstein <110107810+Shlipenstein@users.noreply.github.com>
2022-08-21 23:24:42 +0300

    camelCase

--- src/com/calc/calc.java
+++ src/com/calc/calc.java
@@ -3,11 +3,11 @@ package com.calc;
 import com.calc.Resources.Calculating;
 import com.calc.Resources.InputValue;
 
-public class calc {
+public class Calc {
     public static void main(String[] args) {
         InputValue inputValue = new InputValue();
         inputValue.inputValue();
-        Calculating calculating = new Calculating(inputValue.getfirstNumber(), inputValue.getsecondNumber(), inputValue.getAction());
+        Calculating calculating = new Calculating(inputValue.getFirstNumber(), inputValue.getSecondNumber(), inputValue.getAction());
         calculating.calculate();
         }
 
